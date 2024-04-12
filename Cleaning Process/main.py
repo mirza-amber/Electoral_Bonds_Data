29d0e5a2e55540cdb23aa873384c4061f8f54c70
Amber Mirza <127652335+mirza-amber@users.noreply.github.com>
2024-04-12 15:39:03 +0530

    Add files via upload

---
+++ Cleaning Process/main.py
@@ -0,0 +1,7 @@
+# import tabula
+
+# Remember to download Java and add the \\bin folder to PATH in env variables and install 'jpype' with 'pip'
+
+# dfs = tabula.read_pdf("EB_redemption.pdf", pages='all')
+
+# tabula.convert_into("EB_redemption.pdf", "Redemption_Details.csv", output_format="csv", pages='all', stream=True)
